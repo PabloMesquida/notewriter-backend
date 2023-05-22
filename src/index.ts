31ffa1d5c76b7edc5b00cd9670cd7832fafa976c
Pablo Mesquida <pablomesquida67@gmail.com>
2023-05-22 18:11:29 +0200

    mongo-connection

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,16 @@
+import "dotenv/config";
+import mongoose from "mongoose";
 import express from "express";
 const app = express();
-const port = process.env.PORT || 5000;
+const port = process.env.PORT;
 
 app.get("/", (req, res) => {
   res.send("Hello, World!");
 });
 
-app.listen(port, () => {
-  console.log(`Server runnin on port: ${port}`);
+mongoose.connect(process.env.MONGO_CONNECTION_STRING!).then(() => {
+  console.log("Mongoose connected");
+  app.listen(port, () => {
+    console.log(`Server runnin on port: ${port}`);
+  });
 });
