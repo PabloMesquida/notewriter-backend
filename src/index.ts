7fca2af0e36f86cdf2af3dc82c74ad13c9ce95da
Pablo Mesquida <pablomesquida67@gmail.com>
2023-05-22 18:54:52 +0200

    validate-env

--- src/index.ts
+++ src/index.ts
@@ -1,29 +1,20 @@
 import "dotenv/config";
+import env from "./util/validateEnv";
 import mongoose from "mongoose";
 import express from "express";
 const app = express();
-const port = process.env.PORT || 5000;
+const port = env.PORT;
 
 app.get("/", (req, res) => {
   res.send("Hello, World!");
 });
 
-// try {
-//   mongoose.connect(process.env.MONGO_CONNECTION_STRING!).then(() => {
-//     console.log("Mongoose connected");
-//     app.listen(port, () => {
-//       console.log(`Server running on port: ${port}`);
-//     });
-//   });
-// } catch (error) {
-//   console.error("Error connecting to MongoDB:", error);
-// }
-
-// module.exports = app;
-
-mongoose.connect(process.env.MONGO_CONNECTION_STRING!).then(() => {
-  console.log("Mongoose connected");
-  app.listen(port, () => {
-    console.log(`Server runnin on port: ${port}`);
-  });
-});
+mongoose
+  .connect(env.MONGO_CONNECTION_STRING)
+  .then(() => {
+    console.log("Mongoose connected");
+    app.listen(port, () => {
+      console.log(`Server runnin on port: ${port}`);
+    });
+  })
+  .catch(console.error);
