886756028c736775800b01f9db9eb639fae096c3
Pablo Mesquida <pablomesquida67@gmail.com>
2023-05-22 23:37:44 +0200

    router

--- src/app.ts
+++ src/app.ts
@@ -1,18 +1,10 @@
 import "dotenv/config";
 import express, { NextFunction, Request, Response } from "express";
-import NoteModel from "./models/note";
+import notesRouter from "./routes/notes";
 
 const app = express();
 
-app.get("/", async (req, res, next) => {
-  try {
-    // throw Error("Error!");
-    const notes = await NoteModel.find().exec();
-    res.status(200).json(notes);
-  } catch (error) {
-    next(error);
-  }
-});
+app.use("/api/notes", notesRouter);
 
 app.use((req, res, next) => {
   next(Error("Endpoint not found"));
