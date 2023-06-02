d2566649c8bc75ddec3b20709917392606bb6748
Pablo Mesquida <pablomesquida67@gmail.com>
2023-06-02 20:51:28 +0200

    cors-types

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,7 @@
 import "dotenv/config";
 import express, { NextFunction, Request, Response } from "express";
 import morgan from "morgan";
-import cors from "cors";
+import cors, { CorsOptions } from "cors";
 import notesRoutes from "./routes/notes";
 import userRoutes from "./routes/users";
 import session from "express-session";
@@ -11,15 +11,13 @@ import MongoStore from "connect-mongo";
 
 const app = express();
 
-app.use(cors());
+const corsOptions: CorsOptions = {
+  origin: "*",
+  methods: ["GET", "POST", "PUT", "DELETE"],
+  allowedHeaders: ["Content-Type", "Authorization"],
+};
 
-// Agregar middleware para establecer los encabezados CORS
-app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
-  res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
-  next();
-});
+app.use(cors(corsOptions));
 
 app.use(morgan("dev"));
 
