3accd3d4cb4477b836d5925ed7d19305a055592a
Pablo Mesquida <pablomesquida67@gmail.com>
2023-06-06 00:02:11 +0200

    http-only-false

--- src/app.ts
+++ src/app.ts
@@ -33,7 +33,8 @@ app.use(
     cookie: {
       maxAge: 60 * 60 * 1000,
       sameSite: "none",
-      secure: true,
+      secure: false,
+      httpOnly: false,
     },
     rolling: true,
     store: MongoStore.create({ mongoUrl: env.MONGO_CONNECTION_STRING }),
